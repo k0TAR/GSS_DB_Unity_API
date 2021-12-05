d36bc3909ea8dc4110d86a16607bab4df351ab6f
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2021-12-05 17:29:33 +0900

    can get string in unity.

--- Assets/Scripts/GssGetter.cs
+++ Assets/Scripts/GssGetter.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using MiniJSON;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GssGetter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string URI = "https://script.google.com/macros/s/AKfycbybwPGWrYarv9B6MFL0mW2iozcIVcqvTf6Aa3268uaPn0svEMTRw8D6QSAaZ5W3Ex0B/exec";
+
+    private void Start()
     {
-        
+        StartCoroutine(Get());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Get()
     {
-        
+        UnityWebRequest request = UnityWebRequest.Get($"{URI}?");
+
+        yield return request.SendWebRequest();
+
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            var result = request.downloadHandler.text;
+            Debug.Log(result);
+
+            var response = Json.Deserialize(result);
+            Debug.Log(string.Join(",", response));
+        }
+    }
+
+    [System.Serializable]
+    public class ResponseData
+    {
+        public string userId;
+        public string updateTime;
+        public string playerName;
+        public string message;
     }
 }
