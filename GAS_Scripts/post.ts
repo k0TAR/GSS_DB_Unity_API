eb784d03b8df7c07d043c0dea2399b1f3a0688d8
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2022-02-01 13:15:24 +0900

    save

---
+++ GAS_Scripts/post.ts
@@ -0,0 +1,28 @@
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+function doGet(e: Record<string, unknown>) {
+  const result = executeDoGet(e);
+  return converObjectToJsonString(result);
+}
+
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+function doPost(e: Record<string, unknown>) {
+  const result = executeDoPost(e);
+  return converObjectToJsonString(result);
+}
+
+function executeDoGet(e: Record<string, unknown>) {
+  console.log("start executeDoGet");
+  return { status: "ok", method: "get" };
+}
+
+function executeDoPost(e: Record<string, unknown>) {
+  console.log("start executeDoPost");
+  return { status: "ok", method: "post" };
+}
+
+function converObjectToJsonString(result: Record<string, unknown>) {
+  const payload = ContentService.createTextOutput(
+    JSON.stringify(result)
+  ).setMimeType(ContentService.MimeType.JSON);
+  return payload;
+}
